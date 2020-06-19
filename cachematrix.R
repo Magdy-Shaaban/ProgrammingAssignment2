77c94a02ac33b4bc3ebd23addcbec323804fc085
Magdy-Shaaban <47454206+Magdy-Shaaban@users.noreply.github.com>
2020-06-19 23:33:13 +0200

    assignment with the modifications required
    the assignment required with some modifications

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## assignment for lexical scoping
 
-## Write a short comment describing this function
+
+## creates a special "vector", which is really a list containing a function to
+#1.	set the matrix
+#2.	get the matrix
+#3.	set the inverse of the matrix
+#4.	get the inverse of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -21,7 +26,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##The following function calculates the inverse of the special "matrix" created 
+#with the above function. However, it first checks to see if the inverse has 
+#already been calculated. If so, it gets the inverse from the cache and skips 
+#the computation. Otherwise, it calculates the inverse of the matrix and sets the
+#matrix in the cache via the setinverse function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
